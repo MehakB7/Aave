0dea90a9e491154441ce102376602640078fccc1
MehakB77 <87482443+MehakB7@users.noreply.github.com>
2023-06-23 22:00:18 +0530

    updated purchase method

--- contracts/InterestLottaryAave.sol
+++ contracts/InterestLottaryAave.sol
@@ -8,10 +8,14 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // to purshase ticket we are using dai stable coin
 // and to earn interest we are using ava liquidity pool
 // we will draw the winner after 7 days
+// user need to allow contract to able to transfer money from there account so user need
+// to approve Dai contract ;
 
 contract InterestLottartAava{
     uint drawing;
     uint ticketPrice = 100e18;
+
+    mapping (address=> bool) players;
 
     IPool pool = IPool("0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9");
     IERC20 dai = IERC20("0x6B175474E89094C44Da98b954EedeAC495271d0F");
@@ -23,7 +27,14 @@ contract InterestLottartAava{
         drawing = block.timestamp + 7 days;
      }
 
+// A user can purchase one ticket at max
      function purchase() external{
+        require(!player[msg.sender]);
+        dai.transferFrom(msg.sender, address(this), ticketPrice);
+        player[msg.sender] =true;
+        dai.approve(pool, ticketPrice);
+        pool.deposit(address(this), ticketPrice, address(this), 0);
+
 
      }
 
